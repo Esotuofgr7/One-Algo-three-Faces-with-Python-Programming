88e8aede81cd77703ffe0bafe96797c6161197a4
Esotuofgr7 <kachiesotu@gmail.com>
2025-09-27 18:31:13 +0100

    Add files via upload

---
+++ onealgothreefaces.py
@@ -0,0 +1,12 @@
+def fun1(n):
+    return n*(n+1)/2
+def fun2(n):
+    sum = 0
+    for i in range(1, n+1):
+        sum += 1
+def fun3(n):
+    sum = 0
+    for i in range(1, n+1):
+        for j in range(i, i+1):
+            sum += 1
+        return sum
